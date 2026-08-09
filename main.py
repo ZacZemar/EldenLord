64d5c6648d4bc97e19b009cebeb3683e6b4be627
ZacZemar <zaczemar@gmail.com>
2026-08-08 21:57:46 -0400

    Wire the frost build into CLI.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from query import display_bleed_build, display_madness_build
+from query import display_bleed_build, display_madness_build, display_frost_build
  
 def main():
     choices = ["1","2","3","4","5"]
@@ -9,7 +9,7 @@ def main():
     print("Select a damage type:")
     print("  1. Bleed")
     print("  2. Madness")
-    print("  3. Frost     (Coming Soon)")
+    print("  3. Frost")
     print("  4. Poison    (Coming Soon)")
     print("  5. Lightning (Coming Soon)")
 
@@ -24,8 +24,12 @@ def main():
             display_madness_build()
             break
 
+        if choice == "3":
+            display_frost_build()
+            break
+
         if choice not in choices:
-            print("\nInvalid choice. Please try again and enter a number between 1 and 4.\n")
+            print("\nInvalid choice. Please try again and enter a number between 1 and 5.\n")
             continue
 
         print("\nThis build type is not yet implemented. Check back in a future update, Tarnished.")
